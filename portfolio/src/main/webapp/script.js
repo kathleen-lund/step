c519f59e1dda7eb64d9822a5d7705919ead85ce8
Katie Lund <kathleen.lund8@gmail.com>
2020-05-27 13:14:28 -0500

    Fix code styling.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -30,49 +30,64 @@ function addRandomFact() {
 
 /**
  * Shows the specified blog post to the user
+ * @param {number} postNum The number of which
+ * blog post to show.
  */
 function showBlogPost(postNum) {
+  // Find the correct triangle to expand and switch its image.
+  // Also change the onclick function for this button to hide the post.
   const buttonId = "expandTriangle" + postNum;
   document.getElementById(buttonId).src = "/images/downtriangle.jpeg";
   document.getElementById(buttonId).onclick = function() {
     hideBlogPost(postNum);
   };
+
+  // Add blog post content to the correct area.
   const blogPostId = "blogPostArea" + postNum;
   if (postNum===1) {
-    var pTag = document.createElement("p");
-    pTag.appendChild(document.createTextNode("I have a secret love for \
-      bucket balls. Not only am I randomly good at tossing balls in \
-      incrementally spaced buckets, but I've had lots of opportunity to \
-      do so. Some of my fondest memories are tossing softballs back \
-      into the ball bucket from many yards away at the end of practice, \
-      to the delight of my teammates."));
+      // Append paragraph to DOM.
+    let pTag = document.createElement("p");
+    pTag.appendChild(document.createTextNode('I have a secret love for ' +
+        'bucket balls. Not only am I randomly good at tossing balls in ' +
+        'incrementally spaced buckets, but I\'ve had lots of opportunity to ' +
+        'do so. Some of my fondest memories are tossing softballs back ' +
+        'into the ball bucket from many yards away at the end of practice, ' +
+        'to the delight of my teammates.'));
     document.getElementById(blogPostId).appendChild(pTag);
 
-    var imgTag = document.createElement("img");
+    // Append image to DOM.
+    let imgTag = document.createElement("img");
     imgTag.src = "/images/bucketballs.png";
     document.getElementById(blogPostId).appendChild(imgTag);
   }
   if (postNum===2) {
-    var pTag = document.createElement('p');
-    pTag.appendChild(document.createTextNode("My fingers are very \
-      double-jointed. I can bend them all downward at the first joint \
-      behind the nail, and my thumbs bend backwards in a hitchhiker's thumb \
-      position. This double-jointedness actually makes it a bit difficult \
-      to play the cello, as collapsing joints are a big no-no for your \
-      left hand."));
+    // Append paragraph to DOM.
+    let pTag = document.createElement('p');
+    pTag.appendChild(document.createTextNode('My fingers are very ' +
+        'double-jointed. I can bend them all downward at the first joint ' +
+        'behind the nail, and my thumbs bend backwards in a hitchhiker\'s ' +
+        'thumb position. This double-jointedness actually makes it a bit ' +
+        'difficult to play the cello, as collapsing joints are a big no-no ' + 
+        'for your left hand.'));
     document.getElementById(blogPostId).appendChild(pTag);
   }
 }
 
 /**
  * Hides the specified blog post from the user
+ * @param {number} postNum The number of which
+ * blog post to hide.
  */
 function hideBlogPost(postNum) {
+  // Find the correct triangle to hide and switch its image.
+  // Also change the onclick function for this button to show the post.
   const buttonId = "expandTriangle" + postNum;
   document.getElementById(buttonId).src = "/images/righttriangle.jpeg";
   document.getElementById(buttonId).onclick = function() {
     showBlogPost(postNum);
   };  
+
+  // Remove blog post content from its area.
   const blogPostId = "blogPostArea" + postNum;
   document.getElementById(blogPostId).innerHTML = "";
 }
