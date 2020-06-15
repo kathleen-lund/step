d9a05e6f71636f0f60952e9414dfb3449c66ac9c
Katie Lund <kathleen.lund8@gmail.com>
2020-06-15 22:45:46 +0000

    Remove unnecessary code

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,27 +15,27 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    List<Event> eventsList = new ArrayList<Event>(events);
-    eventsList.stream().sorted((e1, e2) -> e1.getWhen().start() - e2.getWhen().start());
+    // Sort Collection by start time by using a stream
+    events.stream().sorted((e1, e2) -> e1.getWhen().start() - e2.getWhen().start());
+
+    // First track times unavailable for the group
     List<TimeRange> unavailableTimes = new ArrayList<>();
-    for (Event e : eventsList) {
-      boolean eventContainsPerson = false;
-      for (String s : request.getAttendees()) {
-        if (e.getAttendees().contains(s)) {
-          eventContainsPerson = true;
-          break;
-        }
-      }
-      if (eventContainsPerson) {
+    for (Event e : events) {
+      // If the attendee lists are not disjoint (meaning there is at least one attendee in common), 
+      // save this event time as unavailable
+      if (!Collections.disjoint(e.getAttendees(), request.getAttendees())) {
         unavailableTimes.add(e.getWhen());
       } 
     }
+    
     for (int i = 0; i < unavailableTimes.size()-1; i++) {
+      // Check for overlaps and coalesce them into one TimeRange if overlapping
       TimeRange t1 = unavailableTimes.get(i);
       TimeRange t2 = unavailableTimes.get(i+1);
       if (t1.overlaps(t2)) {
@@ -48,9 +48,11 @@ public final class FindMeetingQuery {
         i--;
       }
     }
+
     List<TimeRange> availableTimes = new ArrayList<>();
     long duration = request.getDuration();
     if (!unavailableTimes.isEmpty()) {
+      // Check if the beginning of the day is open
       int firstRange = unavailableTimes.get(0).start();
  
       if (firstRange >= duration) {
@@ -59,25 +61,28 @@ public final class FindMeetingQuery {
     }
     else {
       if (duration <= 1440) {
+        // Entire day is open
         availableTimes.add(TimeRange.fromStartEnd(0, 1440, false));
       }
+      // Duration is longer than possible: return empty list
       return availableTimes;
     }
     
     for (int i = 0; i < unavailableTimes.size() - 1; i++) {
+      // Check if time between the two unavailable times is enough for duration of meeting
       int timeBetween = unavailableTimes.get(i+1).start() - unavailableTimes.get(i).end();
       if (timeBetween >= duration) {
+        // Found an open slot: add to available times
         int newStart = unavailableTimes.get(i).end();
         availableTimes.add(TimeRange.fromStartEnd(newStart, newStart+timeBetween, false));
       }
     }
  
-    if (!unavailableTimes.isEmpty()) {
-      int lastRange = 1440 - unavailableTimes.get(unavailableTimes.size() - 1).end();
-      if (lastRange >= duration) {
-        int newStart = unavailableTimes.get(unavailableTimes.size() - 1).end();
-        availableTimes.add(TimeRange.fromStartEnd(newStart, newStart+lastRange, false));
-      }
+    // Check if last unavailable time to the end of day is long enough
+    int lastRange = 1440 - unavailableTimes.get(unavailableTimes.size() - 1).end();
+    if (lastRange >= duration) {
+      int newStart = unavailableTimes.get(unavailableTimes.size() - 1).end();
+      availableTimes.add(TimeRange.fromStartEnd(newStart, newStart+lastRange, false));
     }
     
     return availableTimes;
